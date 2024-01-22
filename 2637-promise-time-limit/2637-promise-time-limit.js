b78120b78942e01076abe73e7a40441a91d81c34
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-01-22 10:40:31 +0200

    Time: 63 ms (29.03%), Space: 49.5 MB (6.13%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    
+ return async function(...args) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                reject("Time Limit Exceeded")
+            }, t)
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timer);
+                    resolve(result);
+                })
+                .catch((err) => {
+                    clearTimeout(timer);
+                    reject(err);
+                });
+        });
+    };
+};
