0e3b40c70c6e48598464efbb80d85ebba039aeb4
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-01-23 11:04:32 +0200

    Time: 67 ms (14.64%), Space: 49.1 MB (6.51%) - LeetHub

---
+++ 2627-debounce/2627-debounce.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            fn.apply(this, args);
+        }, t);
+    };
+};
