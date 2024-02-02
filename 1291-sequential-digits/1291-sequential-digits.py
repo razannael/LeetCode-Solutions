e5cbea8ea47caa03cf74677c6082ebd1322c0560
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-02-02 15:15:38 +0200

    Time: 33 ms (77.02%), Space: 16.6 MB (63.35%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,12 +1,3 @@
-# To solve this problem, we need to generate all the possible sequential digit
-# numbers and check if they are within the range [low, high].
-# One way to do this is to use a loop that starts from the smallest 
-# possible sequential digit number, which is 12, and ends at the largest
-# possible sequential digit number, which is 123456789. In each iteration,
-# we can append the next digit to the current number and see if it is valid. 
-# If it is, we can add it to the output list.
-# If not, we can skip it and move on to the next number.
-
 
 
 class Solution:
