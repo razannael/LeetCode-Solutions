832f6e0b25891d72d9e9fb791aad4b5c42b867ad
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-02-08 13:39:31 +0200

    Time: 51 ms (67.02%), Space: 48.4 MB (29.27%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let carry = 1;
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i] += carry;
+    if (digits[i] < 10) {
+
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+  return [1, ...digits];
+};
