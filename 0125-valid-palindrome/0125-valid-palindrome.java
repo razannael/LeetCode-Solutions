3b66982017445e6ad8ff2af82de27d78e9d18482
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-03-21 17:37:54 +0200

    Time: 14 ms (37.74%), Space: 44.4 MB (46.86%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int left = 0, right = cleaned.length() - 1;
+        while (left < right) {
+            if (cleaned.charAt(left) != cleaned.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
