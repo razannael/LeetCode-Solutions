e9ae9a7863004a18c24ee09877f1a2bdd2821c44
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-04-09 18:53:27 +0300

    Time: 2 ms (90.34%), Space: 48.4 MB (24.33%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+
+        // Find the middle of the list
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half of the list
+        ListNode prev = null, curr = slow, tmp;
+        while (curr != null) {
+            tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        // Merge the two halves
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+    }
+}
