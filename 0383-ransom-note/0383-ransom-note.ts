2a95e4143470c5a291619a328d488c61711128c1
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2024-12-27 14:03:17 +0200

    Time: 23 ms (27.41%), Space: 54 MB (50.44%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.ts
@@ -0,0 +1,18 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    if(ransomNote.length > magazine.length) return false;
+
+    const magazineTable = {}
+    for(let c of magazine){
+        magazineTable[c] = (magazineTable[c] || 0 )+ 1;
+    }
+    for (let c of ransomNote){
+        if(magazineTable[c] === 0 || magazineTable[c] === undefined) return false;
+        magazineTable[c]--;
+    }
+    return true;
+};
