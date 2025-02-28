b4a69705b23d1f7f316b5ba60484b641d990c5b7
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2025-02-28 22:25:47 +0200

    Time: 0 ms (100%), Space: 19.7 MB (89.88%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(node, minimum, maximum):
+            if not node:
+                return True
+            
+            if not (node.val > minimum and node.val < maximum):
+                return False
+            
+            return valid(node.left, minimum, node.val) and valid(node.right, node.val, maximum)
+        
+        return valid(root, float("-inf"), float("inf"))
