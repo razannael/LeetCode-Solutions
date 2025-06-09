8390caaf19c6ea2a7b9fe04a47f4f2cb2cd0adb9
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2025-06-09 16:55:04 +0300

    Time: 1 ms (34.33%), Space: 17.7 MB (88.06%) - LeetHub

---
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def get_count(prefix, n):
+            count = 0
+            current = prefix
+            next_prefix = prefix + 1
+            while current <= n:
+                count += min(n + 1, next_prefix) - current
+                current *= 10
+                next_prefix *= 10
+            return count
+
+        curr = 1
+        k -= 1  # starting from 1
+
+        while k > 0:
+            count = get_count(curr, n)
+            if count <= k:
+                curr += 1
+                k -= count
+            else:
+                curr *= 10
+                k -= 1
+
+        return curr
