4ccd9a023db3d83efc973fcd4ad467a03d838d6e
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2025-11-04 19:50:51 +0200

    Time: 0 ms (100%), Space: 17.7 MB (83.45%) - LeetHub

---
+++ 1006-clumsy-factorial/1006-clumsy-factorial.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def clumsy(self, n):
+       if n == 1:
+            return 1
+       elif n == 2:
+            return 2
+       elif n == 3:
+            return 6
+       elif n == 4:
+            return 7
+       else:
+            if n % 4 == 0:
+                return n + 1
+            elif n % 4 <= 2:
+                return n + 2
+            else:
+                return n - 1
