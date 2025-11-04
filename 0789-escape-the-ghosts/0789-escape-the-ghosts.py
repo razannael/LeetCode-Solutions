64728aa8f80ea616d8682ab475091f6c78f07ab5
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2025-11-04 19:27:01 +0200

    Time: 0 ms (100%), Space: 18 MB (20.42%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -0,0 +1,12 @@
+import math
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        def distance(i,j,x,y):
+            return abs(i-x)+abs(j-y)
+        thresh=distance(0,0,target[0],target[1])
+        # print(thresh)
+        for i in ghosts:
+            # print(distance(i[0],i[1],target[0],target[1]))
+            if distance(i[0],i[1],target[0],target[1])<=thresh:
+                return False
+        return True
