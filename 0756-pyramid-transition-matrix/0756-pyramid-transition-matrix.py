f506a74e6abac4c9cbb9ad9f4d580e6c90561b0c
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2025-12-29 19:49:27 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -1,33 +1,23 @@
-from collections import defaultdict
-from functools import lru_cache
-
 class Solution:
-    def pyramidTransition(self, bottom: str, allowed: list[str]) -> bool:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
         mp = defaultdict(list)
         for s in allowed:
             mp[s[:2]].append(s[2])
-
-        @lru_cache(None)
-        def canBuild(row: str) -> bool:
+        memo = {}
+        def canBuild(row :str) -> bool:
             if len(row) == 1:
                 return True
-
-            # build next row with backtracking
             def build(i: int, nxt: list[str]) -> bool:
                 if i == len(row) - 1:
                     return canBuild("".join(nxt))
-
                 pair = row[i:i+2]
                 if pair not in mp:
                     return False
-
-                for ch in mp[pair]:
-                    nxt.append(ch)
-                    if build(i + 1, nxt):
+                for c in mp[pair]:
+                    nxt.append(c)
+                    if build(i+1 , nxt):
                         return True
                     nxt.pop()
                 return False
-
             return build(0, [])
-
         return canBuild(bottom)
