8ac681c270fc1f1173f0df40b82189e520ff45b2
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2026-04-17 16:36:14 +0300

    Time: 248 ms (67.96%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,3 +1,8 @@
 # Write your MySQL query statement below
-SELECT * FROM cinema WHERE (id MOD 2 = 1) AND (description != 'boring')
-ORDER BY rating DESC;
+
+SELECT *
+FROM cinema c
+WHERE c.id % 2 =1 
+    AND 
+    c.description != 'boring'
+ORDER BY c.rating DESC;
