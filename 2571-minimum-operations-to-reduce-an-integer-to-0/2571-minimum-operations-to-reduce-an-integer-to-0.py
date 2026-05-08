08dd99f0f848373019ca5288f090202095f3a8c6
Razan Tuhaimer <127951072+razannael@users.noreply.github.com>
2026-05-08 21:24:43 +0300

    Time: 4 ms (7.4%), Space: 19 MB (98.73%) - LeetHub

---
+++ 2571-minimum-operations-to-reduce-an-integer-to-0/2571-minimum-operations-to-reduce-an-integer-to-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        cnt = 0
+        while n:
+            exp = 1
+            while 2**exp < n: exp += 1
+            n = min(abs(2**exp - n), abs(2**(exp-1) - n))
+            cnt += 1
+        return cnt
